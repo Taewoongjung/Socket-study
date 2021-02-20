ba8626b02e3e59b84cea75b25265676e06bf8977
tae <aipooh8882@naver.com>
2021-02-20 16:31:21 +0900

    interactive web server

--- socket.js
+++ socket.js
@@ -1,23 +1,23 @@
-const webSocket = require('ws');
+const WebSocket = require('ws');
 
 module.exports = (server) => {
     const wss = new WebSocket.Server({ server });
 
-    wss.on('connection', (ws, req) => {
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    wss.on('connection', (ws, req) => { // 웹소켓 연결 시 
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress; // ['x-forwarded-for'] 가 있는 이유 => 프록시 서버가 있을 때 ip가 변조될 수 있는 위험이 있기 때문
         console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => {
+        ws.on('message', (message) => { // client로부터 메시지 
             console.log(message);
         });
-        ws.on('error', (error) => {
+        ws.on('error', (error) => { // 에러 시
             console.error(error);
         });
-        ws.on('close', () => {
+        ws.on('close', () => { // 연결 종료 시
             console.log('클라이언트 접속 해제', ip);
             clearInterval(ws.interval);
         });
 
-        ws.interval = setInterval(() => {
+        ws.interval = setInterval(() => {  // 3초마다 클라이언트로 메시지 전송
             if (ws.readyState === ws.OPEN) {
                 ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
             }
