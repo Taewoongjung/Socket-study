a0867da8dc655fefe895ad58882e593ddc28c453
tae <aipooh8882@naver.com>
2021-02-22 21:53:59 +0900

    chat with graphic messages

--- socket.js
+++ socket.js
@@ -1,24 +1,38 @@
 const SocketIO = require('socket.io');
+const axios = require('axios');
+const cookieParser = require('cookie-parser');
+const cookie = require('cookie-signature');
 
-module.exports = (server) => {
+module.exports = (server, app, sessionMiddleware) => {
   const io = SocketIO(server, { path: '/socket.io' });
+  app.set('io', io);
+  const room = io.of('/room');
+  const chat = io.of('/chat');
 
-  io.on('connection', (socket) => { // 웹소켓 연결 시
+  io.use((socket, next) => {
+    cookieParser(process.env.COOKIE_SECRET)(socket.request, socket.request.res, next);
+    sessionMiddleware(socket.request, socket.request.res, next);
+  });
+
+  room.on('connection', (socket) => {
+    console.log('room 네임스페이스에 접속');
+    socket.on('disconnect', () => {
+      console.log('room 네임스페이스 접속 해제');
+    })
+  });
+
+  chat.on('connection', (socket) => {
+    console.log('chat 네임스페이스에 접속');
     const req = socket.request;
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
-    socket.on('disconnect', () => { // 연결 종료 시
-      console.log('클라이언트 접속 해제', ip, socket.id);
-      clearInterval(socket.interval);
+    const { headers: { referer } } = req;
+    const roomId = referer
+      .split('/')[referer.split('/').length - 1]
+      .replace(/\?.+/, '');
+    socket.join(roomId);
+
+    socket.on('disconnect', () => {
+      console.log('chat 네임스페이스 접속 해제');
+      socket.leave(roomId);
     });
-    socket.on('error', (error) => { // 에러 시
-      console.error(error);
-    });
-    socket.on('reply', (data) => { // 클라이언트로부터 메시지
-      console.log(data);
-    });
-    socket.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
-      socket.emit('news', 'Hello Socket.IO');
-    }, 3000);
   });
 };
